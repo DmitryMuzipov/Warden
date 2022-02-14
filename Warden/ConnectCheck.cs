eca29cf66e80841aa01da8468f2b2c30d9902005
R3dChet <Vendigo331@gmail.com>
2022-02-14 10:54:30 +0700

    Добавьте файлы проекта.

---
+++ Warden/ConnectCheck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warden
+{
+    class ConnectCheck
+    {
+        private Dictionary<string, string> CheckConnect(string ip)
+        {
+            Ping ping = new Ping();
+            PingReply PingReply = ping.Send(ip);
+
+            var connect = new Dictionary<string, string>()
+            {
+                ["time"] = PingReply.RoundtripTime.ToString(),
+                ["status"] = PingReply.Status.ToString(),
+                ["address"] = PingReply.Address.ToString(),
+            };
+
+            return connect;
+        }
+
+        public void ConnectViev(string ip)
+        {
+            Dictionary<string, string> connect = CheckConnect(ip);
+
+            Console.WriteLine("время ответа: " + connect["time"]);
+            Console.WriteLine("Статус: " + connect["status"]);
+            Console.WriteLine("ip адресс: " + connect["address"]);
+        }
+    }
+}
---
+++ Warden/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectCheck connect = new ConnectCheck();
+
+            connect.ConnectViev("91.109.200.200");
+        }
+    }
+}
