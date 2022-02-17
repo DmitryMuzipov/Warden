fde2dd2c5cae6bb993c5c71470d90f98d90b071f
R3dChet <Vendigo331@gmail.com>
2022-02-17 16:46:44 +0700

    Настроено подключение к базе данных.

--- Warden/Program.cs
+++ Warden/Program.cs
@@ -11,8 +11,11 @@ namespace Warden
         static void Main(string[] args)
         {
             ConnectCheck connect = new ConnectCheck();
-
-            connect.ConnectViev("10.225.20.38");
+            workDB inBD = new workDB();
+                     
+            connect.ConnectViev("10.225.0.35");
+            inBD.SQLquery("select * from ABANDONNED_FOUND LIMIT 100;");
         }
     }
 }
+//"10.225.20.38"
---
+++ Warden/workDB.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OracleClient;
+
+namespace Warden
+{
+    class workDB
+    {
+        string addres = "Data Source=Ronyx; User id=onyx; Password=onyx;";
+        private void connectDB()
+        {
+            OracleConnection connection = new OracleConnection(addres);
+            connection.Open();
+
+            Console.WriteLine("подключение произведено\n");
+        }
+
+        private void closeDB()
+        {
+            OracleConnection connection = new OracleConnection(addres);
+            connection.Close();
+
+            Console.WriteLine("подключение завершено\n");
+        }
+
+        public void SQLquery(string query)
+        {
+            connectDB();
+
+            OracleConnection connection = new OracleConnection(addres);
+            OracleCommand command = connection.CreateCommand();
+            command.CommandText = query;
+
+            Console.WriteLine("Зпрос к базе данных выполнен\n");
+            closeDB();
+        }
+    }
+}
+//
+//"Data Source=Prototype; User id=cds; Password=cds;"
