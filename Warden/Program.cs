1f0c22207c88dd34e550d8dfc1d946897a449ee9
R3dChet <Vendigo331@gmail.com>
2022-02-18 16:13:15 +0700

    Добавление класса для работы с электронной почтой

--- Warden/Program.cs
+++ Warden/Program.cs
@@ -10,11 +10,14 @@ namespace Warden
     {
         static void Main(string[] args)
         {
-            ConnectCheck connect = new ConnectCheck("10.225.0.35");
-            workDB inBD = new workDB("Data Source=Ronyx; User id=onyx; Password=onyx;");
+            SendMail send = new SendMail();
+            //ConnectCheck connect = new ConnectCheck("10.225.0.35");
+            //workDB inBD = new workDB("Data Source=Ronyx; User id=onyx; Password=onyx;");
                      
-            connect.ConnectViev();
-            inBD.SQLquery("select * from ABANDONNED_FOUND LIMIT 100;");
+            //connect.ConnectViev();
+            //inBD.SQLquery("select * from ABANDONNED_FOUND LIMIT 100;");
+
+            send.Send();
         }
     }
 }
---
+++ Warden/SendMail.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace Warden
+{
+    class SendMail
+    {
+        public SendMail()
+        {
+
+        }
+        public void Send()
+        {
+            // получаем SmtpClient
+            SmtpClient smtpClient = GetSmtpClient();
+            // получаем сообщение
+            MailMessage mailMessage = GetMailMessage();
+
+            // отправляем сообщение
+            smtpClient.Send(mailMessage);
+        }
+
+        private MailMessage GetMailMessage()
+        {
+            // создаём объект сообщения
+            MailMessage mailMessage = new MailMessage();
+            // указываем, от кого отсылается сообщение
+            mailMessage.From = new MailAddress("muckredchet@gmail.com");
+            // указываем, кому отправляется сообщение
+            mailMessage.To.Add(new MailAddress("Vendigo331@gmail.com"));
+            // указываем тему сообщения
+            mailMessage.Subject = "Отчет по табличному пространству";
+            // указываем текст сообщения
+            mailMessage.Body = "Текст сообщения";
+
+            // добавляем вложение
+            //mailMessage.Attachments.Add(new Attachment(@"C:\\test.txt"));
+
+            return mailMessage;
+        }
+
+        private SmtpClient GetSmtpClient()
+        {
+            // создаём объект SmtpClient
+            SmtpClient smtpClient = new SmtpClient();
+            // указываем адрес используемого SMTP сервера
+            smtpClient.Host = "smtp.gmail.com";
+            // указываем, что необходимо использовать SSL
+            smtpClient.EnableSsl = true;
+            // задаём используемый порт
+            smtpClient.Port = 587;
+            // доступ к SMTP серверу
+            smtpClient.Credentials = new NetworkCredential("muckredchet@gmail.com", "wvxovkcwburfgpbm");
+
+            return smtpClient;
+        }
+
+    }
+}
