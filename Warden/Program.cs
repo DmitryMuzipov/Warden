7c9a5b4531aa92980b106203d36707291e7485c8
Diman Redchet <Vendigo331@gmail.com>
2022-11-30 15:50:42 +0700

    Рефакторинг Main класса. Перенос всех данных в переменные.

--- Warden/Program.cs
+++ Warden/Program.cs
@@ -11,17 +11,23 @@ namespace Warden
     {
         static void Main(string[] args)
         {
+            // Сервер
+            string ip = "10.225.0.35";
+            string connectionString = "User id=cds; Password=cds; Server=Prototype;";
+            // Запрос
+            string query = "select * from AA";
+            // Письмо
             string from = "MuzipovDR@tomskneft.ru";
             string to = "MuzipovDR@tomskneft.ru";
             string subject = "Тестовый заголовок";
             string body = "Тестовое сообщение";
 
 
-            ConnectCheck connect = new ConnectCheck("10.225.0.35");
+            ConnectCheck connect = new ConnectCheck(ip);
             connect.ConnectViev();
 
-            workDB inBD = new workDB("User id=cds; Password=cds; Server=Prototype;");
-            inBD.SQLquery("select * from AA");
+            workDB inBD = new workDB(connectionString);
+            inBD.SQLquery(query);
 
             SendMail send = new SendMail(from, to, subject, body);
             send.Send();
